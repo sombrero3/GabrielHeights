2817125faeb6c05bd5b7e9873516ba13bad76e41
Carmel <sombrero3@walla.com>
2023-04-20 13:30:42 +0300

    profile adjustmente

--- client/src/PagesContainer/Home/Profile/Profile.js
+++ client/src/PagesContainer/Home/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ScrollService from '../../../utilities/ScrollService'
+import phonecall from '../../../assets/Home/phonecall.png'
 import './Profile.css'
 
 function Profile() {
@@ -11,11 +12,11 @@ function Profile() {
                 <div className='profile-parent'>
                     <div className='profile-details'>
                         <div className='profile-details-background'></div>
-                        <div className="profile-call-us">
+                        {/* <div className="profile-call-us">
                             <span>
                                 צלצלו אלינו <span className='profile-phone-number'>050-2006233</span>
                             </span>
-                        </div>
+                        </div> */}
                         <div className='profile-main-text'>
                             <span>
                                 <h1>
@@ -28,14 +29,26 @@ function Profile() {
                                 מתמחים באיטום ושיקום מבנים בגובה על ידי סנפלינג, במות הרמה, ופיגומים
                             </span>
                         </div>
-                        <div className='profile-options'>
-                            <button
-                                className='btn profile-btn'
-                                onClick={() => ScrollService.scrollHandler.scrollToContactMe()}>
-                                <span></span>
-                                {""}
-                                צרו קשר{" "}
-                            </button>
+                        <div className='profile-buttons-row'>                            
+                            <div className='profile-options'>
+                                <button
+                                    className='btn profile-mail-btn'
+                                    onClick={() => ScrollService.scrollHandler.scrollToContactMe()}>
+                                    <span></span>
+                                    {""}
+                                    שליחת מייל{" "}                                    
+                                </button>
+                            </div>
+                            <div className='profile-options'>
+                                <button
+                                    className='btn profile-call-btn'
+                                    onClick={() => ScrollService.scrollHandler.scrollToContactMe()}>
+                                    <span></span>
+                                    {""}
+                                    050-2006233{" "}
+                                    <img className='profile-phone-logo' src={phonecall} alt="phone call icon" />
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
--- client/src/utilities/References/References.js
+++ client/src/utilities/References/References.js
@@ -3,31 +3,37 @@ import whatsapp from '../../assets/Home/whatsapp.png'
 import phonecall from '../../assets/Home/phonecall.png'
 import facebook from '../../assets/Home/facebook.png'
 import email from '../../assets/Home/email.png'
+import instagram from '../../assets/Home/instagram.png'
 import './References.css'
-export default function References(){
+export default function References() {
 
-    return (
-        <div className='references-container'>
-             <a href='#ContactMe'>
-              <i className='email-icon'>
-                <img src={email} alt="phone call icon" />
-              </i>
-            </a>
-            <a href='https://www.facebook.com/carmel.hershtik/'>
-              <i className='facebook-icon'>
-                <img src={facebook} alt="facebook icon" />
-              </i>
-            </a>
-            <a href="https://wa.me/+972502006233">
-              <i className='whatsapp-icon'>
-                <img src={whatsapp} alt="whatsapp icon" />
-              </i>
-            </a>
-            <a href='tel:+972502006233'>
-              <i className='phonecall-icon'>
-                <img src={phonecall} alt="phone call icon" />
-              </i>
-            </a>
-        </div>
-    )
+  return (
+    <div className='references-container'>
+      <a href='#ContactMe'>
+        <i className='email-icon'>
+          <img src={email} alt="phone call icon" />
+        </i>
+      </a>
+      <a href='https://instagram.com/gabrielheights?igshid=YmMyMTA2M2Y='>
+        <i className='instagram-icon'>
+          <img src={instagram} alt="instagram icon" />
+        </i>
+      </a>
+      <a href='https://www.facebook.com/profile.php?id=100091627946986&mibextid=LQQJ4d'>
+        <i className='facebook-icon'>
+          <img src={facebook} alt="facebook icon" />
+        </i>
+      </a>
+      <a href="https://wa.me/+972502006233">
+        <i className='whatsapp-icon'>
+          <img src={whatsapp} alt="whatsapp icon" />
+        </i>
+      </a>
+      <a href='tel:+972502006233'>
+        <i className='phonecall-icon'>
+          <img src={phonecall} alt="phone call icon" />
+        </i>
+      </a>
+    </div>
+  )
 }
